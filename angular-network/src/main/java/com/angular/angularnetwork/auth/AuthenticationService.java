bbbd5e57baec3b66784bbc9fc736bfd07c542511
Murat Kaan Aydoğdu <aydogdumq@gmail.com>
2025-02-03 18:31:05 +0300

    reg request

---
+++ angular-network/src/main/java/com/angular/angularnetwork/auth/AuthenticationService.java
@@ -0,0 +1,10 @@
+package com.angular.angularnetwork.auth;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+    }
+}
