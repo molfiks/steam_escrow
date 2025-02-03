381aba9661121d7482e274e9d091c3a423b079a8
Murat Kaan Aydoğdu <aydogdumq@gmail.com>
2025-02-03 21:32:21 +0300

    email service

--- angular-network/src/main/java/com/angular/angularnetwork/auth/AuthenticationService.java
+++ angular-network/src/main/java/com/angular/angularnetwork/auth/AuthenticationService.java
@@ -1,10 +1,87 @@
 package com.angular.angularnetwork.auth;
 
 
+import com.angular.angularnetwork.email.EmailService;
+import com.angular.angularnetwork.email.EmailTemplateName;
+import com.angular.angularnetwork.role.RoleRepository;
+import com.angular.angularnetwork.user.Token;
+import com.angular.angularnetwork.user.TokenRepository;
+import com.angular.angularnetwork.user.User;
+import com.angular.angularnetwork.user.UserRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
-    public void register(RegistrationRequest request) {
+
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+    @Value("${application.mailling.frontend.activation-url}")
+    private String activationUrl;
+
+
+    public void register(RegistrationRequest request) throws MessagingException {
+        var userRole = roleRepository.findByName("USER")
+                // todo - better exception handling
+                .orElseThrow(() -> new IllegalStateException("User role not found"));
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSaveActivationToken(user);
+        emailService.sendEmail(
+                user.getEmail(),
+                user.fullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account activation"
+        );
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        // generate token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+
+        for(int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length()); // 0..9
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
     }
 }
--- angular-network/src/main/java/com/angular/angularnetwork/auth/RegistrationRequest.java
+++ angular-network/src/main/java/com/angular/angularnetwork/auth/RegistrationRequest.java
@@ -1,5 +1,9 @@
 package com.angular.angularnetwork.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,4 +12,19 @@ import lombok.Setter;
 @Setter
 @Builder
 public class RegistrationRequest {
+
+    @NotEmpty(message = "Firstname is mandatory")
+    @NotBlank(message = "Firstname is mandatory")
+    private String firstname;
+    @NotEmpty(message = "Lastname is mandatory")
+    @NotBlank(message = "Lastname is mandatory")
+    private String lastname;
+    @Email(message = "Email is not formatted")
+    @NotEmpty(message = "Email name is mandatory")
+    @NotBlank(message = "Email name is mandatory")
+    private String email;
+    @Size(min = 8, message = "Password should be 8 characters long minimum")
+    @NotEmpty(message = "Password name is mandatory")
+    @NotBlank(message = "Password name is mandatory")
+    private String password;
 }
